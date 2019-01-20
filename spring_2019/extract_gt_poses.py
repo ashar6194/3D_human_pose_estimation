c407c7d340ed54634e1f9f2211617c43f2cd25ee
ashar6194 <ashar.ali.06@gmail.com>
2019-01-20 17:09:13 -0500

    Clean up with training data generatorsa and gt pose creation

--- spring_2019/extract_gt_poses.py
+++ spring_2019/extract_gt_poses.py
@@ -8,51 +8,56 @@ from mpl_toolkits.mplot3d import Axes3D
 # import tensorflow as tf
 from scipy import io
 import glob
+import time
 
 root_dir = '/media/mcao/Miguel/UBC_hard/train/'
+#
+# joint_names = ['HeadPGX','HipsPGX', 'LeftArmPGX', 'LeftFingerBasePGX', 'LeftFootPGX', 'LeftForeArmPGX', 'LeftHandPGX','LeftInHandIndexPGX',
+#                'LeftInHandThumbPGX', 'LeftLegPGX', 'LeftShoulderPGX', 'LeftToeBasePGX', 'LeftUpLegPGX','Neck1PGX', 'NeckPGX','RightArmPGX',
+#                'RightFingerBasePGX','RightFootPGX', 'RightForeArmPGX', 'RightHandPGX', 'RightInHandIndexPGX', 'RightInHandThumbPGX',
+#                'RightLegPGX', 'RightShoulderPGX', 'RightToeBasePGX', 'RightUpLegPGX', 'Spine1PGX', 'SpinePGX']
+#
+# idx_list = [0, 13, 23, 26, 27, 25, 22, 17, 24, 9, 4, 11, 18, 19, 16, 5, 6, 3]
+# joint_name_list = [joint_names[i] for i in idx_list]
 
-joint_names = ['HeadPGX','HipsPGX', 'LeftArmPGX', 'LeftFingerBasePGX', 'LeftFootPGX', 'LeftForeArmPGX', 'LeftHandPGX','LeftInHandIndexPGX',
-               'LeftInHandThumbPGX', 'LeftLegPGX', 'LeftShoulderPGX', 'LeftToeBasePGX', 'LeftUpLegPGX','Neck1PGX', 'NeckPGX','RightArmPGX',
-               'RightFingerBasePGX','RightFootPGX', 'RightForeArmPGX', 'RightHandPGX', 'RightInHandIndexPGX', 'RightInHandThumbPGX',
-               'RightLegPGX', 'RightShoulderPGX', 'RightToeBasePGX', 'RightUpLegPGX', 'Spine1PGX', 'SpinePGX']
 
-idx_list = [0, 13, 23, 26, 27, 25, 22, 17, 24, 9, 4, 11, 18, 19, 16, 5, 6, 3]
-joint_name_list = [joint_names[i] for i in idx_list]
-
-
-def visualize_pose(pose_array):
-  fig = plt.figure(1)
-  ax = fig.add_subplot(111, projection='3d')
-  ax.scatter(pose_array[:, 0], pose_array[:, 1], pose_array[:, 2])
+# def visualize_pose(pose_array):
+#   fig = plt.figure(1)
+#   ax = fig.add_subplot(111, projection='3d')
+#   ax.scatter(pose_array[:, 0], pose_array[:, 1], pose_array[:, 2])
 
 
 def main():
   instance_list = glob.glob('%s*/groundtruth.mat' % root_dir)
   flag_instance = False
+  useful_jnt_idx = pickle.load(open('lookup.pkl', 'rb'))
 
-  for inst_idx, instance in tqdm(enumerate(sorted(instance_list))):
-
-    fig = plt.figure()
+  for instance in tqdm(sorted(instance_list)):
+    mat_file = io.loadmat(instance)
+    aa = time.time()
+    vid_idx = instance.split('/')[-2]
+    # fig = plt.figure()
     pose_list = []
 
     for idx in range(1001):
-      mat_file = io.loadmat(instance)
       # Create a dictionary of all existing joint names
-      if not flag_instance:
-        print 'Hello!'
-        flag_instance = True
-        idx_dict = {}
-        type_dict = str(mat_file['joints'][inst_idx][0].dtype).strip().split('), (')
-        for limb_idx, limb_loc in enumerate(type_dict):
-          j_name = limb_loc.strip().split(',')[-2][1:-1]
-          idx_dict[j_name] = limb_idx
-
-        useful_jnt_idx = [idx_dict[name] for name in joint_name_list]
+      # if not flag_instance:
+      #   print 'Hello!'
+      #   flag_instance = True
+      #   idx_dict = {}
+      #   type_dict = str(mat_file['joints'][0][0].dtype).strip().split('), (')
+      #   for limb_idx, limb_loc in enumerate(type_dict):
+      #     j_name = limb_loc.strip().split(',')[-2][1:-1]
+      #     idx_dict[j_name] = limb_idx
+      #   useful_jnt_idx = [idx_dict[name] for name in joint_name_list]
+      #   pickle.dump(useful_jnt_idx, open('lookup.pkl', 'wb'))
 
       joints = np.array([mat_file['joints'][0][idx][0][0][jnt_idx][0][12:15] for jnt_idx in useful_jnt_idx])
       pose_list.append(joints)
 
-    pickle.dump(np.array(pose_list), open('%s%d/gt_poses.pkl' % (root_dir, (inst_idx+1)), 'wb'))
+
+    pickle.dump(np.array(pose_list), open('%s%s/gt_poses.pkl' % (root_dir, vid_idx), 'wb'))
+    # print time.time() - aa
 
 
 if __name__ == '__main__':
---
+++ spring_2019/tdg.py
@@ -0,0 +1,81 @@
+import math
+import glob
+import pickle
+import numpy as np
+import os
+import keras
+import cv2
+
+# Change argument source file based on the dataset
+from ubc_args import args
+
+
+class DataGenerator(keras.utils.Sequence):
+
+    def __init__(self, list_ids, batch_size=10, dim=(10, 16, 2),
+                 dim_visual=(10, 16, 565), num_actions=3, shuffle=True,
+                 dim_loc=(10, 3), dim_ego=(10, 6), time_steps=10, flag_data='flow_kron', num_blocks=1):
+
+        self.dim = dim
+        self.num_actions = num_actions
+        self.dim_visual = dim_visual
+        self.batch_size = batch_size
+        self.list_ids = list_ids
+        self.shuffle = shuffle
+        self.indexes = []
+        self.flag_data = flag_data
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.floor(len(self.list_ids) / self.batch_size))
+
+    def __getitem__(self, index):
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        X, y = [], []
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_ids[k] for k in indexes]
+
+        # Generate data
+        if self.flag_data == 'flow_kron':
+            X, y = self.__data_generation_kron(list_IDs_temp, feature_dir=args.root_dir)
+
+        return X, y
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_ids))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation_kron(self, list_ids_temp, feature_dir):
+        X = np.empty((self.batch_size, 100, 100, 3))
+        y = np.empty((self.batch_size, 18, 3), dtype=float)
+        # y = [[None]] * self.batch_size
+
+        for idx, id_name in enumerate(list_ids_temp):
+          img = cv2.imread(id_name)
+          img = cv2.resize(img, (100, 100))
+          X[idx, ] = img
+          name_parse = id_name.split('/')
+
+          img_idx = int(name_parse[-1].split('.')[1]) - 1
+          vid_idx = name_parse[-5]
+          gt_file = '%s%s/gt_poses.pkl' % (feature_dir, vid_idx)
+
+          gt_pose = pickle.load(open(gt_file, 'r'))[img_idx, ]
+          y[idx, ] = gt_pose
+
+        return X, y
+
+
+if __name__ == '__main__':
+  # a = '/media/mcao/Miguel/UBC_hard/train/1/images/depthRender'
+  img_list = sorted(glob.glob('%s*/images/depthRender/Cam1/*.png' % args.root_dir))
+  print len(img_list)
+  # list_ids = pickle.load(open(list_id_file, 'rb'))
+
+  qwe = DataGenerator(img_list, flag_data='flow_kron', batch_size=32)
+  a, b = qwe.__getitem__(0)
+  # print a, b
+  print a[0].shape, b[0].shape
