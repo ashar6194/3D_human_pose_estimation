397ee2add03c3a3b2b8306bbeef864401e5c319a
ashar6194 <ashar.ali.06@gmail.com>
2019-01-29 15:14:54 -0500

    Modifications in eval pipeline

--- spring_2019/eval_pipeline.py
+++ spring_2019/eval_pipeline.py
@@ -10,7 +10,7 @@ from model_set import build_ddp_basic, compile_network, build_ddp_vgg, build_min
 
 
 def eval_results(args, directory=args.test_dir):
-  pred_cam_list = sorted(glob.glob('%s*/pred_%s_%s.pkl' % (directory, args.model_name, args.cam_type)))
+  pred_cam_list = sorted(glob.glob('%s*/ddpdh_pred_%s_%s.pkl' % (directory, args.model_name, args.cam_type)))
   gt_pose_list = sorted(glob.glob('%s*/gt_poses.pkl' % directory))
 
   full_pred_list = []
@@ -31,6 +31,42 @@ def eval_results(args, directory=args.test_dir):
   print np.mean(error)
   print np.median(error)
 
+
+def infer_outputs_dh(args, model, directory=args.test_dir):
+
+  datastats = pickle.load(open('datastats.pkl', 'rb'))
+  train_mean = datastats['mean']
+  train_std = datastats['std']
+
+  img_folder_list = sorted(glob.glob('%s*/images/depthRender/Cam1' % directory))
+  for id_folder in tqdm(img_folder_list):
+    img_name_list = sorted(glob.glob('%s/*.png' % id_folder))
+
+    pose_list = []
+    for id_name in img_name_list:
+      # print id_name
+
+      img = cv2.imread(id_name)
+      img = cv2.resize(img, (args.input_size, args.input_size))
+      dst = np.zeros(shape=(5, 2))
+      img = cv2.normalize(img, dst, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+      img = np.expand_dims(np.mean(img, axis=-1), axis=-1)
+      X = np.expand_dims(img, axis=0)
+      final_pred = model.predict(X)
+
+      pose_list.append(final_pred)
+
+    pose_npy = np.squeeze(np.array(pose_list))
+    pose_npy = (pose_npy * train_std) + train_mean
+    pose_npy = pose_npy.reshape((-1, 18, 3))
+    # print pose_npy.shape
+
+    name_parse = id_folder.split('/')
+    vid_idx = name_parse[-4]
+    cam_idx = name_parse[-1]
+    pred_file = '%s%s/ddpdh_pred_%s_%s.pkl' % (directory, vid_idx, args.model_name, cam_idx)
+    # print pred_file
+    pickle.dump(pose_npy, open(pred_file, 'wb'))
 
 
 def infer_outputs(args, model, directory=args.test_dir):
@@ -78,10 +114,10 @@ def infer_outputs(args, model, directory=args.test_dir):
 
 
 if __name__ == '__main__':
-  # ckpt_dir = '/media/mcao/Miguel/UBC_hard/' + 'keras_models/'
-  # model_name = ckpt_dir + 'ddp_mini_vgg_cam1_2019_01_29.h5'
-  # inp_shape = (args.input_size, args.input_size, 1)
-  # ddp_model = build_minivgg_basic(inp_shape, num_classes=100)
-  # ddp_model.load_weights(model_name)
-  # infer_outputs(args, ddp_model)
+  ckpt_dir = '/media/mcao/Miguel/UBC_hard/' + 'keras_models/'
+  model_name = ckpt_dir + 'ddpdh_mini_vgg_ep5_01_29.h5'
+  inp_shape = (args.input_size, args.input_size, 1)
+  ddp_model = build_minivgg_basic(inp_shape, num_classes=54)
+  ddp_model.load_weights(model_name)
+  infer_outputs_dh(args, ddp_model)
   eval_results(args, args.test_dir)
