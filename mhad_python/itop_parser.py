4bff981c5b64a936e6c15ec7e2e57abfd4a3ee29
ashar6194 <ashar.ali.06@gmail.com>
2019-02-05 22:23:55 -0500

    cleaning Itop parser

--- mhad_python/itop_parser.py
+++ mhad_python/itop_parser.py
@@ -3,7 +3,8 @@ import hdf5storage
 import numpy as np
 import cv2
 
-from tqdm import  tqdm
+from sys import getsizeof
+from tqdm import tqdm
 
 base_path = '/data/ITOP Dataset/itop_mats/'
 itopset = h5py.File('/data/ITOP Dataset/ITOP_side_test_depth_map.h5', 'r+')
@@ -11,12 +12,14 @@ itop_pose = h5py.File('/data/ITOP Dataset/ITOP_side_test_labels.h5', 'r+')
 keypoints = itop_pose['real_world_coordinates']
 
 dp_img = itopset['data']
-keypoints = itop_pose['real_world_coordinates']
-keypoints  = np.array(keypoints).astype(np.float32)
-hdf5storage.write(keypoints, path='/poses', filename=base_path+'gt_poses.mat', matlab_compatible=True)
+# keypoints = itop_pose['real_world_coordinates']
+# keypoints  = np.array(keypoints).astype(np.float32)
+# hdf5storage.write(keypoints, path='/poses', filename=base_path+'gt_poses.mat', matlab_compatible=True)
 
 dp_img = np.array(dp_img).astype(np.float32)
 dp_img[dp_img > 3.3] = 8
+
+dp_img_arr = []
 
 # rescaled_img = np.interp(dp_img, (dp_img.min(), dp_img.max()), (0, +1))
 
@@ -34,7 +37,10 @@ for i in tqdm(range(dp_img.shape[0])):
   # cv2.imshow('im_name', img)
   # cv2.waitKey(1)
 
-  hdf5storage.write(img, path='/depth_image', filename=base_path+'itop_img_%07d.mat' % i,
-                    matlab_compatible=True)
+  dp_img_arr.append(img)
+
+print getsizeof(dp_img_arr)
+
+hdf5storage.write(np.array(dp_img_arr), path='/depth_image', filename=base_path+'all_dp_img.mat', matlab_compatible=True)
 
 
